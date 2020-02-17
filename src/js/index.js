10fc0ef4496f93e26d261c7c2880ab81ebc1cc29
JuanAcosta <juan.acosta@10pearls.com>
2020-02-17 08:26:54 -0500

    hot module replacement

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,12 @@
 import '../css/index.css';
+import text from './text';
 
-document.body.innerHTML=('<p>Hola mundo desde webpack! home</p>');
+text();
+
+if (module.hot){
+    module.hot.accept('./text.js', function () {
+        console.log('He recargado en caliente');
+        text();
+    })
+}
+
---
+++ src/js/text.js
@@ -0,0 +1,5 @@
+function text() {
+    document.body.innerHTML=('<p>Hola si ves este texto por 5 segundos algo pasará</p>');
+}
+
+export default text;
