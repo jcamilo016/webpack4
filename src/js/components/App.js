b550816ed1d3908ee0abf593d4b72d523be9d24e
JuanAcosta <juan.acosta@10pearls.com>
2020-02-18 17:22:41 -0500

    Adicionando imagenes, videos y fuentes

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import data from '../../../data';
 import Loader from './Loader';
+import logo from '../../../url-loader/images/platzi.png';
+import video from '../../../url-loader/video/que-es-core.mp4'
 
 function App () {
     const [loaderList, setLoaderList] = React.useState([]);
@@ -11,6 +13,10 @@ function App () {
     return (
         <div>
             <p>Que linda aplicación en React.js</p>
+            <video src={video} width={360} controls poster={logo}/>
+            <div>
+                <img src={logo} alt="logo" width={100}/>
+            </div>
             <ul>
                 {
                     loaderList.map((loader) => {
